0882afad3808cb91c09b5f6ce4d2137a29ad5145
briankmbugua <briankmbuguak@gmail.com>
2023-07-14 19:41:07 +0300

    used this file to add some initial data to the database

---
+++ init_db.py
@@ -0,0 +1,19 @@
+from app import app, db, Post, Comment
+
+
+# make sure to import the app instance for this to work
+with app.app_context():
+    post1 = Post(title='Post the first', content='Content for the first post')
+    post2 = Post(title='Post The Second',
+                 content='Content for the Second post')
+    post3 = Post(title='Post The Third', content='Content for the third post')
+
+    comment1 = Comment(content='Comment for the first post', post=post1)
+    comment2 = Comment(content='Comment for the second post', post=post2)
+    comment3 = Comment(
+        content='Another comment for the second post', post_id=2)
+    comment4 = Comment(content='Another comment for the first post', post_id=1)
+
+    db.session.add_all([post1, post2, post3])
+    db.session.add_all([comment1, comment2, comment3, comment4])
+    db.session.commit()
